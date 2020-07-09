6e048a03b61e0e820b90ea26093e983b599b70db
Ankan Bose <49253833+ankanbose848@users.noreply.github.com>
2020-07-09 21:42:04 +0530

    Update trapezoidalRule.c

--- trapezoidalRule.c
+++ trapezoidalRule.c
@@ -1,13 +1,15 @@
+/* Integration of f(x) = x^2 */
+
 #include <stdio.h>
 #include <math.h>
 float f(float x)
 {
-    return (1 / (1 + pow(x, 2)));
+    return (x*x);
 }
 int main()
 {
     int i, n;
-    float a, b, h, s = 0, t;
+    float a, b, h, sum = 0, t;
     printf("\n Enter the lower limit:");
     scanf("%f", &a);
     printf("\n Enter the upper limit:");
@@ -15,8 +17,9 @@ int main()
     printf("\n Enter the number of sub intervals:");
     scanf("%d", &n);
     h = (b - a) / n;
-    for (i = 1; i < n - 1; i++)
-        s = s + f(a + i * h);
-    t = (h / 2) * (f(a) + 2 * s + f(b));
-    printf("\n The result is %6.4f\n", t);
-}
+    do {
+	sum = sum + (h/2)*(f(a) + f(a+h));
+	a = a + h;
+    } while(a<b);
+    printf("\n The result is %6.4f\n", sum);
+}    
